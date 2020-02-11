3ebe41304c60d584a1fcf4837cf4fd55fb443160
oscar1055 <56590181+oscar1055@users.noreply.github.com>
2020-02-11 13:44:37 +0100

    FAK
    DET FUNKAR I<ne

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private State state;
+    private enum State
+    {
+        Normal,
+    }
+    void Update()
+    {
+        switch (state)
+        {
+            default:
+            case State.Normal:
+
+                break;
+        }
+    }
+    void movement()
+    {
+
+    }
+    public
+    void jump()
+    {
+
+    }
+}
