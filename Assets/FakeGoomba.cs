38d485869080367779e2bf0446f271078ae2e775
oscar1055 <56590181+oscar1055@users.noreply.github.com>
2020-03-10 11:54:31 +0100

    hej
    hej

--- Assets/FakeGoomba.cs
+++ Assets/FakeGoomba.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class FakeGoomba : MonoBehaviour
 {
     [SerializeField] [Range(1, 100)] float speed = 15;
+    [SerializeField] [Range(1, 100)] float jumpHeight = 1;
     public float detectRange = 2f;
+    public LayerMask ground;
+
+    bool isGrounded;
     void Update()
     {
+        isGrounded = Physics2D.Raycast(transform.position, -transform.up, .1f);
         transform.position += -transform.right * Time.deltaTime * speed;
-        if (Physics2D.Raycast(transform.position, -transform.right,detectRange))
+        if (Physics2D.Raycast(transform.position, -transform.right, detectRange, ground) && isGrounded) 
         {
             Jump();
         }
     }
     void Jump()
     {
-
+        GetComponent<Rigidbody2D>().velocity = transform.up * jumpHeight;
     }
 }
