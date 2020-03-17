f57a592bc18db6c5499df49a759b7f3c602fb2fe
oscar1055 <56590181+oscar1055@users.noreply.github.com>
2020-03-17 10:53:37 +0100

    yes
    yes

--- Assets/EnemyShooting.cs
+++ Assets/EnemyShooting.cs
@@ -5,15 +5,23 @@ public class EnemyShooting : MonoBehaviour
 {
     public GameObject projectile;
     GameObject newProjectile;
+    bool isShooting = false;
     void Update()
     {
-        StartCoroutine(cd());
+        
+
+        if (!isShooting)
+        {
+            StartCoroutine(cd());
+        }
     }
     IEnumerator cd()
     {
+        isShooting = true;
         yield return new WaitForSeconds(2f);
         newProjectile = Instantiate(projectile);
         newProjectile.transform.position = new Vector2(transform.position.x, transform.position.y + Random.Range(-1, 2));
         newProjectile.GetComponent<Rigidbody2D>().velocity = -transform.right * Time.deltaTime * 50f;
+        isShooting = false;
     }
 }
